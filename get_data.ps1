b0ae0fa3584b4172252035944102ff5389ec3b19
conjectures <alexis.othonos@gmail.com>
2021-06-18 18:30:24 +0100

    added filters

--- get_data.ps1
+++ get_data.ps1
@@ -9,18 +9,44 @@
 Function Test-Func {
   [CmdletBinding(DefaultParameterSetName="Pipeline")]
   Param(
-    [Parameter(ValueFromPipeline=$true, ParameterSetName="Pipeline", Position=0)] [System.Array] $Data
+    [Parameter(ValueFromPipeline=$true, ParameterSetName="Pipeline", Position=0)] [System.Array] $Data,
 
-    # [Parameter(ParameterSetName="Direct", Position=0)] [string] $Filename
+    [Parameter(ParameterSetName="Direct", Position=0)] [string] $Filename,
+    [Parameter()] [Alias("Where")] [ValidatePattern("^(\w+=[^\'`, ],)*(\w+=[^\'`, ]*)$")] [string[]] $Filter
   )
   
-  # if ($PSBoundParameters.ContainsKey('Filename')){
-  #   Write-Host "TEST"
-  # }
-  Process {
-    foreach ($row in $Data){
-      $row.where({$_.gender -match 'M'})
+  begin {
+    # Parse Filters If passed as argument
+    if ($PSBoundParameters.ContainsKey('Filter')){
+      Write-Verbose "Parse filter parameters"
+      $filters = @()
+      foreach ($string in $Filter){ 
+        $field,$query = $string.Split('=')
+	$filters += "(`$_.$field -match '$query')"
+      }
+      $fs = [scriptblock]::Create($filters -join ' -and ')
+      #Write-Verbose $filters
+	
     }
+
+    if ($PSBoundParameters.ContainsKey('Filename')){
+      Write-Host $Filename
+      $Data = Import-Csv $Filename
+    }
+    # $Data.where({$_.gender -match 'M'})
+  }
+  Process {
+    # $_.where({($_.gender -match 'M') -and ($_.name -match 'M')})
+    # $_ | Where-Object { ($_.gender -match 'M') -and ($_.name -match 'M') }
+    # $_ | ? $fs
+    $_.where($fs)
+	
+    # foreach ($row in $Data){
+    #   $row.where({$_.gender -match 'M'})
+    # }
+  }
+  end {
+    # $Data.where({$_.gender -match 'M'})
   }
 
 }
