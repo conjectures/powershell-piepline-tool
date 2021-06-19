3078a83f7a7fe37b9548d08bde81c0ad2e91ffa5
conjectures <alexis.othonos@gmail.com>
2021-06-19 19:14:47 +0100

    added hashing algorithm

--- README.md
+++ README.md
@@ -3,3 +3,15 @@
 ## Index
 1. Documentation
 
+
+## Documentation
+Functions: 
+- Apply-Filter
+  Used to apply a filter to the data
+
+- Get-RandomAlphanumericString
+  Used to get a salt (random alphanumeric string) for the hashing 
+
+- Get-HashValue
+  Apply  a one-way hashing algorithm to a string with 
+
--- get_data.ps1
+++ get_data.ps1
@@ -6,7 +6,7 @@
 # 2. Test-Func -Filename <filename> [-Filter, -Apply, ...]
 
 
-Function Test-Func {
+Function Apply-Filter{
   [CmdletBinding(DefaultParameterSetName="Pipeline")]
   Param(
     [Parameter(ValueFromPipeline=$true, ParameterSetName="Pipeline", Position=0)] [System.Array] $Data,
@@ -53,4 +53,44 @@ Function Test-Func {
 
 # Import-Csv .\data.csv 
 
+# Function credit to https://gist.github.com/marcgeld/4891bbb6e72d7fdb577920a6420c1dfb
+Function Get-RandomAlphanumericString {
+  [CmdletBinding()] [alias("Get-Alphanumeric")]
+  Param( [int] $length = 8)
+  Process{
+    # Create a list of available characters in hex  - feed into get random to choose $lenght characters and output
+    Write-Output ( -join ((0x30..0x39) + (0x41..0x5A) + (0x61..0x7a) | Get-Random -Count $length | % {[char]$_}) )
+  }
+}
+
+# Function to hash a
+Function Get-HashValue {
+  [CmdletBinding()]
+  Param(
+    [Parameter(Mandatory=$true, ValueFromPipeline=$true)] [string[]] $Data,
+
+    [Parameter()] [string] $Algorithm = 'sha256'
+  )
+  Begin { 
+    $HashingAlgorithm = [System.Security.Cryptography.HashAlgorithm]::Create($Algorithm)
+    $result = @()
+  }
+  Process {
+      # Generate salt
+      $salt = Get-RandomAlphanumericString -Length 10
+      Write-Verbose "salt: $salt"
+      # Hash string
+      Write-Verbose "string $Data"
+      $hash = $HashingAlgorithm.ComputeHash([System.Text.Encoding]::UTF8.GetBytes($Data + $salt))
+      $hashString = [System.BitConverter]::ToString($hash).Replace('-','')
+
+      $result += @{
+        HashValue=$hashString
+        Salt=$salt
+      }
+  }
+  End {
+    $result
+  }
+}
 
